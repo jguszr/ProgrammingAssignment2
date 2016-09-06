064938aae9d13fa741d3eb16074abbb0fff8f012
Gus <jguszr@gmail.com>
2016-09-05 21:26:01 -0300

    Working, still missing some comments
    pretty much like the vector example

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Week3 assigniment by Jose Gustavo Z. Rosa (jguszr@gmail.com)
-## those two functions are based on the matrix inversion with cache sutf 
+## those two functions are based on the matrix inversion with cache sutf
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  print("foobar")
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function()
+    x
+  setinverse <- function(solve)
+    m <<- solve
+  getinverse <- function()
+    m
+  list(
+    set = set,
+    get = get,
+    setinverse = setinverse,
+    getinverse = getinverse
+  )
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if (!is.null(m)) {
+    message("Probably there is some cached data hanging here...")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data) %*% data
+  x$setinverse(m)
+  m
+  
 }
